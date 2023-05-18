f73c70c1f349ed2eac608eb688d15e44bd0ae28c
NOMPILO <124359100+Nkazee@users.noreply.github.com>
2023-05-18 23:16:14 +0200

    Formatted date

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Sartuday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `Last updated on ${day}, ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   console.log(response.data);
   let descriptionElement = document.querySelector("#description");
@@ -5,11 +28,13 @@ function showTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let speedElement = document.querySelector("#speed");
   let cityElement = document.querySelector("#city");
+  let dateElement = document.querySelector("#date");
   descriptionElement.innerHTML = response.data.weather[0].description;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   humidityElement.innerHTML = response.data.main.humidity;
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   cityElement.innerHTML = response.data.name;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "ee9f98438ce01b9f7ae2c52f40add8ae";
