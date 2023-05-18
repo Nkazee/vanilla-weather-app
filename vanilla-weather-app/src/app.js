5cca4c5a6587ef0baef1d8bf49c530ff92edab97
NOMPILO <124359100+Nkazee@users.noreply.github.com>
2023-05-18 23:57:06 +0200

    Updated relevant weather icon

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -29,16 +29,22 @@ function showTemperature(response) {
   let speedElement = document.querySelector("#speed");
   let cityElement = document.querySelector("#city");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   descriptionElement.innerHTML = response.data.weather[0].description;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   humidityElement.innerHTML = response.data.main.humidity;
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   cityElement.innerHTML = response.data.name;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
+let city = "Durban";
 let apiKey = "ee9f98438ce01b9f7ae2c52f40add8ae";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bloemfontein&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(showTemperature);
